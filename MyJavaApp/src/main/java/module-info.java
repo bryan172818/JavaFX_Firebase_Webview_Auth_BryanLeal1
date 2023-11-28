6a041374f84c0aaf925fb1d3077a6086244e2686
bryan172818 <142602413+bryan172818@users.noreply.github.com>
2023-11-27 20:29:24 -0500

    Delete MyJavaApp directory

--- MyJavaApp/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.myjavaapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens com.example.myjavaapp to javafx.fxml;
-    exports com.example.myjavaapp;
-}
